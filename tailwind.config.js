89ee60c95a1ced57ba3311e2a25d5dd287ca7887
pxMahio <ron.wollschlaeger@frontshape.de>
2023-10-25 23:02:04 +0200

    Upload challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ["./*.{html,js}"],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      'Kumbh-Sans': ['Kumbh Sans', 'sans-serif'],
+    },
     extend: {
       animation: {
         'spin-slow': 'spin 3s linear infinite',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        'html': { fontSize: "18px" },
+      })
+    }),
+  ],
 };
